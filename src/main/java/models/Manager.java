900de728478db5f14ebaf03e0624e51036fe1217
David A Pears <davidapears@gmail.com>
2018-07-12 11:49:31 +0100

    manager class created

--- src/main/java/models/Manager.java
+++ src/main/java/models/Manager.java
@@ -1,4 +1,63 @@
 package models;
 
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.zip.Adler32;
+
+@Entity
+@Table(name ="managers")
+
 public class Manager {
+    private int id;
+    private double budget;
+    private Department department;
+    private List<Admin> admin;
+
+
+    public Manager(){}
+
+    public Manager(int id, double budget, Department department, List<Admin> admin) {
+        this.id = id;
+        this.budget = budget;
+        this.department = department;
+        this.admin = admin;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "budget")
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+    @Column(name="department")
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+   @Column(name= "admin")
+    public List<Admin> getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(List<Admin> admin) {
+        this.admin = admin;
+    }
 }
